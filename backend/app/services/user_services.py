953d0492e19263c807c42e3cc36ca21605b8e214
Finn <112250336+finn200102@users.noreply.github.com>
2025-03-14 16:54:34 +0100

    Implement user_services CRUD Operations

---
+++ backend/app/services/user_services.py
@@ -0,0 +1,56 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import func, desc
+from app.models.novel import Novel
+from app.models.author import Author
+from app.models.library import Library
+from app.models.user import User
+from app.models.tag import Tag
+from app.models.genre import Genre
+from app.models.chapter import Chapter
+
+def create_user(db: Session, user_data: dict):
+
+    user = User(**user_data)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+def get_user_by_username(db: Session, username):
+    """
+    Returns Users that match that username
+    """
+
+    users = db.query(User).filter(User.username == username)
+    return users
+
+
+def delete_user(db: Session, user_id):
+    """
+    Deletes a User from database
+    """
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+
+
+def update_user(db: Session, user_id, user_data):
+    """
+    Update the User with User_data which can be complete new data or just part of it
+    """
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        return None
+
+    # Update User attributes
+    for key, value in user_data.items():
+        if hasattr(user, key):
+            setattr(user, key, value)
+
+    db.commit()
+    db.refresh(user)
+    return user
