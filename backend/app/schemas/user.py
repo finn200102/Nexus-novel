843c46d30166185bc7f03cab10f36e9ceed26922
Finn <112250336+finn200102@users.noreply.github.com>
2025-03-14 18:09:06 +0100

    Implement User Schemas

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    username: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserLogin(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class LoginResponse(BaseModel):
+    id: int
+    username: str
+    message: str
+
+    class Config:
+        orm_mode = True
