cf044516b83460aab301a817c52a8904323c71ab
Finn <112250336+finn200102@users.noreply.github.com>
2025-03-15 22:53:26 +0100

    Test: get_novels, get_novel_by_id routes

--- backend/tests/routes/test_novel.py
+++ backend/tests/routes/test_novel.py
@@ -11,7 +11,7 @@ def test_add_novel(client, db):
     novel_data = {"url": url}
 
     # Make request
-    response = client.post("/novel/add", json=novel_data)
+    response = client.post("/novel/", json=novel_data)
 
     # Assertions
     assert response.status_code == status.HTTP_201_CREATED
@@ -20,4 +20,74 @@ def test_add_novel(client, db):
     db_novel = get_novel_by_url(db, url).first()
     assert db_novel is not None
     assert db_novel.url == url
-    
+
+
+def test_get_novels(client, db):
+    # Test data
+    url = "https://www.royalroad.com/fiction/21220/mother-of-learning"
+    novel_data = {"url": url}
+    novel_title = "Mother of Learning"
+    author_name =  "nobody103"
+
+    # Make request
+    response = client.post("/novel/", json=novel_data)
+
+    # Assertions
+    assert response.status_code == status.HTTP_201_CREATED
+
+    # Verify nove was created
+    db_novel = get_novel_by_url(db, url).first()
+    assert db_novel is not None
+    assert db_novel.url == url
+
+    # get novel
+    response = client.get("/novel/")
+    print("Response status:", response.status_code)
+    print("Response JSON:", response.json())
+
+
+    # Assertions for the GET request
+    assert response.status_code == 200
+    novels = response.json()
+    assert novels is not None
+    assert len(novels) > 0
+
+    # Verify the created novel is in the response
+    found = False
+    for novel in novels:
+        if novel["url"] == url:
+            found = True
+            break
+    assert found, "Created novel not found in the response"
+
+
+
+def test_get_novel_by_id(client, db):
+    # Test data
+    url = "https://www.royalroad.com/fiction/21220/mother-of-learning"
+    novel_data = {"url": url}
+    novel_title = "Mother of Learning"
+    author_name =  "nobody103"
+
+    # Make request
+    response = client.post("/novel/", json=novel_data)
+
+    # Assertions
+    assert response.status_code == status.HTTP_201_CREATED
+
+    # Verify nove was created
+    db_novel = get_novel_by_url(db, url).first()
+    assert db_novel is not None
+    assert db_novel.url == url
+
+    # get novel
+    response = client.get(f"/novel/{db_novel.id}")
+    print("Response status:", response.status_code)
+    print("Response JSON:", response.json())
+
+
+    # Assertions for the GET request
+    assert response.status_code == 200
+    novel = response.json()
+    assert novel is not None
+    assert novel["id"] == db_novel.id
