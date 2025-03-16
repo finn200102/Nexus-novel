3271c65b9290dfdf2d192390ce6a51a63f1a1c3f
Finn <112250336+finn200102@users.noreply.github.com>
2025-03-16 13:51:54 +0100

    chore: update novel router test with now mandetory libary_id

--- backend/tests/routes/test_novel.py
+++ backend/tests/routes/test_novel.py
@@ -3,12 +3,16 @@ from fastapi import status
 import bcrypt
 from app.schemas.user import UserCreate
 from app.services.novel_services import get_novel_by_url
+from app.services.library_services import create_library
 
 
 def test_add_novel(client, db):
     # Test data
     url = "https://www.royalroad.com/fiction/21220/mother-of-learning"
     novel_data = {"url": url}
+    # create lib
+    library = create_library(db, library_data={"name": "lib1"})
+    novel_data["library_id"] = library.id
 
     # Make request
     response = client.post("/novel/", json=novel_data)
@@ -26,6 +30,9 @@ def test_get_novels(client, db):
     # Test data
     url = "https://www.royalroad.com/fiction/21220/mother-of-learning"
     novel_data = {"url": url}
+    # create lib
+    library = create_library(db, library_data={"name": "lib1"})
+    novel_data["library_id"] = library.id
     novel_title = "Mother of Learning"
     author_name =  "nobody103"
 
@@ -66,6 +73,9 @@ def test_get_novel_by_id(client, db):
     # Test data
     url = "https://www.royalroad.com/fiction/21220/mother-of-learning"
     novel_data = {"url": url}
+    # create lib
+    library = create_library(db, library_data={"name": "lib1"})
+    novel_data["library_id"] = library.id
     novel_title = "Mother of Learning"
     author_name =  "nobody103"
 
