381c3020f7e0a8d5b670790d96150276e4fc54b3
Finn <112250336+finn200102@users.noreply.github.com>
2025-03-29 14:42:26 +0100

    Implement: addNovel

--- frontend/src/services/novelService.ts
+++ frontend/src/services/novelService.ts
@@ -1,6 +1,11 @@
 import apiClient from "./apiClient";
 import axios from "axios";
 
+interface CreateNovelSchema {
+  url: string;
+  library_id: number;
+}
+
 export const novelService = {
   async getAllNovels(library_id: number) {
     try {
@@ -13,6 +18,22 @@ export const novelService = {
       throw error;
     }
   },
+  async addNovel(library_id: number, url: string) {
+    try {
+      const data: CreateNovelSchema = {
+        url: url,
+        library_id: library_id,
+      };
+      const response = await apiClient.post(`/novel/`, data);
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status != 401) {
+        this.handleError(error, "Error fetching libraries");
+      }
+      throw error;
+    }
+  },
+
   handleError(error: any, message: string) {
     console.error(message + ":", error);
 
